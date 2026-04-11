254c10f0d9e296470e8492fb040cee25cf981840
Erosyonz <alleahidelapena@gmail.com>
2026-04-11 19:39:50 +0800

    add css

---
+++ src/main/java/com/example/dashboard/HelloApplication.java
@@ -0,0 +1,19 @@
+package com.example.dashboard;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 734, 513);
+        stage.setTitle("Dashboard-admin");
+        stage.setScene(scene);
+        stage.show();
+    }
+}
