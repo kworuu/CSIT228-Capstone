bd85c0776c29e01daf55d340addbf4dbeea68967
Vonn <vonnvincentcaballero@gmail.com>
2026-05-07 00:19:11 +0800

    Changes on the naming conventions

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,16 +3,17 @@ module com.example.dashboard_admin {
     requires javafx.fxml;
     requires javafx.web;
 
-
     opens com.example.dashboard_admin to javafx.fxml;
     exports com.example.dashboard_admin;
 
     // Ensure these match your folder names letter-for-letter
-    opens com.example.dashboard_admin.HelperClasses to javafx.fxml;
-    exports com.example.dashboard_admin.HelperClasses;
+    opens com.example.dashboard_admin.helper_classes to javafx.fxml;
+    exports com.example.dashboard_admin.helper_classes;
 
-    exports com.example.dashboard_admin.Views;
-    opens com.example.dashboard_admin.Views to javafx.fxml;
-    exports com.example.dashboard_admin.MapLogic;
-    opens com.example.dashboard_admin.MapLogic to javafx.fxml;
+    exports com.example.dashboard_admin.views;
+    opens com.example.dashboard_admin.views to javafx.fxml;
+
+    exports com.example.dashboard_admin.map_logic;
+    opens com.example.dashboard_admin.map_logic to javafx.fxml;
+
 }
