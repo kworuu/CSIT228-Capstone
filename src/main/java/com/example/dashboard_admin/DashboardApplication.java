749f14528a158943e7d5ffeb3d260e0b2b3608b2
Vonn <vonnvincentcaballero@gmail.com>
2026-05-07 00:15:50 +0800

    Proper naming convention -- Evacuation.fxml -> evacuation.fxml

--- src/main/java/com/example/dashboard_admin/DashboardApplication.java
+++ src/main/java/com/example/dashboard_admin/DashboardApplication.java
@@ -15,7 +15,7 @@ public class DashboardApplication extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(DashboardApplication.class.getResource("Evacuation.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(DashboardApplication.class.getResource("evacuation.fxml"));
         Parent root = fxmlLoader.load();
 
         //stage.initStyle(StageStyle.UNDECORATED);
