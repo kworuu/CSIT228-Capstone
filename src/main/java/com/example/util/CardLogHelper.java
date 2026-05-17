40729b53680f9c072c019e0e6f411a176c4858dc
Jexoh1738 <villagonzalomark1@gmail.com>
2026-05-17 22:41:16 +0800

    Major changes

---
+++ src/main/java/com/example/util/CardLogHelper.java
@@ -0,0 +1,16 @@
+package com.example.util;
+
+import com.example.model.InventoryItem;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.Separator;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.VBox;
+
+public class CardLogHelper {
+}
